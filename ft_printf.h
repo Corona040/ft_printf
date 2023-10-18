6f2c57a9d158ea3c8db8a3b4a3a40bfc38afc3e4
Eduardo Corona Oliveira Costa <ecorona-@student.42porto.com>
2023-10-18 19:09:17 +0100

    lestart

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ecorona- <ecorona-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 23:18:42 by ecorona-          #+#    #+#             */
-/*   Updated: 2023/10/18 12:32:16 by ecorona-         ###   ########.fr       */
+/*   Updated: 2023/10/18 19:08:38 by ecorona-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,5 @@
 
 int	ft_putubase_fd(char *set, long unsigned int n, int fd);
 int	ft_printf(const char *format, ...);
-
-typedef union u_data
-{
-	unsigned int		u;
-	unsigned long int	lu;
-}	t_data;
-
-typedef struct s_flist
-{
-	t_data	d;
-	void	(*f)();
-}	t_flist;
 
 #endif
--- old2_ft_printf.c
+++ old2_ft_printf.c
@@ -6,22 +6,71 @@
 /*   By: ecorona- <ecorona-@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 10:24:47 by ecorona-          #+#    #+#             */
-/*   Updated: 2023/10/18 19:08:44 by ecorona-         ###   ########.fr       */
+/*   Updated: 2023/10/18 18:59:20 by ecorona-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	get_type(const char **f)
+{
+	if (f && *f)
+	{
+		if (**f == '%')
+		{
+			(*f)++;
+			if (**f == 'c' || **f == 'd' || **f == 'i'
+					|| **f == 'u' || **f == 'x' || **f == 'X')
+			{
+				(*f)++;
+				return (1);
+			}
+			else if (**f == 's' || **f == 'p')
+			{
+				(*f)++;
+				return (2);
+			}
+			else
+				return (-2);
+		}
+		else
+		{
+			(*f)++;
+			return (0);
+		}
+	}
+	else
+		return (-1);
+}
+
+typedef unsigned long int	lu;
+
 int	ft_printf(const char *format, ...)
 {
+	int		fd;
+	t_data	data;
 	va_list	ap;
+	lu		a;
 
+	a = (lu)
+	printchar((char) a);
+	printhex((int) a);
+
+	fd = 1;
 	va_start(ap, format);
-	//DO SOMETHING
-	va_end(ap);
+	while (*format)
+	{
+		if (get_type(&format) == 1)
+			data.u = va_arg(ap, unsigned int);
+		else if (get_type(&format) == 2)
+			data.lu = va_arg(ap, unsigned long int);
+	}
+	return (0);
 }
 
 int	main(void)
 {
-	ft_printf("banananana");
+	t_data	data;
+
+	data.u = 100;
 }
